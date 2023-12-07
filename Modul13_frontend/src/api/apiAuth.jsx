7fca1ae2930c95445e5247d54a0d8eaaf8db19dd
Rafaelbilly <rafaelbilly6@gmail.com>
2023-12-07 20:59:02 +0700

    first commit

---
+++ Modul13_frontend/src/api/apiAuth.jsx
@@ -0,0 +1,19 @@
+import useAxios from ".";
+const SignUp = async (data) => {
+  try {
+    const response = await useAxios.post("/register", data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+const SignIn = async (data) => {
+  try {
+    const response = await useAxios.post("/login", data);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
+
+export { SignUp, SignIn };
---
+++ Modul13_frontend/src/components/forms/InputFloatingForm.jsx
@@ -0,0 +1,18 @@
+import { Form, FloatingLabel } from "react-bootstrap";
+
+const InputFloatingForm = (props) => {
+  
+  return (
+    <Form.Group className="mb-3" controlId={props.name}>
+      <FloatingLabel className="fw-bold text-light" label={props.label}>
+        <Form.Control
+          className="text-light bg-transparent border-secoondary"
+          placeholder={props.placeholder || props.label}
+          {...props}
+        />
+      </FloatingLabel>
+    </Form.Group>
+  );
+};
+
+export default InputFloatingForm;
